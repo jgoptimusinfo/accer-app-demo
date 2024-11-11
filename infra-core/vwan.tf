a92adfcd800ac5bdb075969a59ccab1f7a3062fe
jgoptimusinfo <155094924+jgoptimusinfo@users.noreply.github.com>
2024-11-11 16:15:57 -0300

    Update address spaces in vwan.tf: change to 172.16.0.0/16 range and comment out modules (#8)

--- infra-core/vwan.tf
+++ infra-core/vwan.tf
@@ -1,7 +1,7 @@
 ## --------------------------------------------------------
 ##  Network - Main
 ## --------------------------------------------------------
-
+/* 
 module "vnet_spoke_aks" {
   source = "../modules/network"
 
@@ -9,17 +9,17 @@ module "vnet_spoke_aks" {
   prefix_name         = "acn-spoke-aks"
   resource_group_name = module.rg_main_network.name
 
-  address_space = ["10.200.1.0/24"]
+  address_space = ["172.16.1.0/24"]
   subnets       = {
       appgw_snet = {
         name                                          = "appgw"
-        address_prefixes                              = ["10.200.1.0/25"]
+        address_prefixes                              = ["172.16.1.0/25"]
         private_endpoint_network_policies             = "Disabled"
         private_link_service_network_policies_enabled = true
       },
       aks_snet = {
         name                                          = "aks"
-        address_prefixes                              = ["10.200.1.128/25"]
+        address_prefixes                              = ["172.16.1.128/25"]
         private_endpoint_network_policies             = "Disabled"
         private_link_service_network_policies_enabled = true
       }
@@ -30,7 +30,7 @@ module "vnet_spoke_aks" {
 
   tags = var.tags
 }
-
+ */
 ## --------------------------------------------------------
 ##  Virtual WAN
 ## --------------------------------------------------------
@@ -48,9 +48,10 @@ resource "azurerm_virtual_hub" "virtual_hub" {
   virtual_wan_id      = azurerm_virtual_wan.virtual_wan.id
   address_prefix      = "10.200.0.0/22"
 }
-
+/* 
 resource "azurerm_virtual_hub_connection" "spoke_aks_vhub" {
   name                      = "spoke-aks-vhub"
   virtual_hub_id            = azurerm_virtual_hub.virtual_hub.id
   remote_virtual_network_id = module.vnet_spoke_aks.vnet_id
 }
+ */
