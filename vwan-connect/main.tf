62dc519e27b948c22f0a01a761fbb192ee9186ce
jgoptimusinfo <155094924+jgoptimusinfo@users.noreply.github.com>
2024-11-12 17:17:44 -0300

    Add comments to local variables in firewall and vwan-connect configurations for clarity (#19)

--- vwan-connect/main.tf
+++ vwan-connect/main.tf
@@ -1,7 +1,7 @@
 ## --------------------------------------------------------
 ##  locals
 ## --------------------------------------------------------
-
+/* 
 locals {
   virtual_hub_id = "/subscriptions/3124b81f-32b6-49f2-98d6-7500ef2a165f/resourceGroups/accern-nwsvcs-prd-eus2-rg/providers/Microsoft.Network/virtualHubs/acn-virtual-hub-prd"
   spoke_prd_aks_vnet_id = "/subscriptions/3124b81f-32b6-49f2-98d6-7500ef2a165f/resourceGroups/accern-nwsvcs-prd-eus2-rg/providers/Microsoft.Network/virtualNetworks/acn-spoke-aks-vnet"
@@ -19,4 +19,4 @@ resource "azurerm_virtual_hub_connection" "spoke_prd_aks_vhub" {
   remote_virtual_network_id = local.spoke_prd_aks_vnet_id
 }
 
- 
+  */
