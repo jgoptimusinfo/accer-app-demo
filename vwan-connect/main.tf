e0b6a5efb6ccd32e38e1703096f9a0318e75fe96
jgoptimusinfo <javier.gaibisso@optimusinfo.com>
2024-11-12 11:57:15 -0300

    Add Terraform workflows for VWAN connection and unit tests, and initialize configuration files

---
+++ vwan-connect/main.tf
@@ -0,0 +1,24 @@
+## --------------------------------------------------------
+##  locals
+## --------------------------------------------------------
+
+data "azurerm_client_config" "current" {}
+
+locals {
+  virtual_hub_id = "/subscriptions/3124b81f-32b6-49f2-98d6-7500ef2a165f/resourceGroups/accern-nwsvcs-prd-eus2-rg/providers/Microsoft.Network/virtualHubs/acn-virtual-hub-prd"
+  spoke_prd_aks_vnet_id = "/subscriptions/3124b81f-32b6-49f2-98d6-7500ef2a165f/resourceGroups/accern-nwsvcs-prd-eus2-rg/providers/Microsoft.Network/virtualNetworks/acn-spoke-prd-aks-vnet"
+}
+
+## --------------------------------------------------------
+##  Network - Virtual hub Connections
+## --------------------------------------------------------
+
+# Spoke PRD-AKS to Virtual Hub
+ 
+resource "azurerm_virtual_hub_connection" "spoke_prd_aks_vhub" {
+  name                      = "spoke-prd-aks-vhub"
+  virtual_hub_id            = local.virtual_hub_id
+  remote_virtual_network_id = local.spoke_prd_aks_vnet_id
+}
+
+ 
---
+++ vwan-connect/terraform.tfvars
@@ -0,0 +1,12 @@
+## ------------------------------------
+##  COMMON VARIABLES
+## ------------------------------------
+
+location = "eastus2"
+
+tags = {
+  "Environment" = "prod"
+  "Owner"       = "Javier.Gaibisso"
+  "Application" = "accern-core"
+  "ID"          = "00000001"
+}
