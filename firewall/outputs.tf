6fe2834fc173f4c8377f59a9f4a3ff9cccc615ac
jgoptimusinfo <javier.gaibisso@optimusinfo.com>
2024-11-13 16:08:19 -0300

    Add separate outputs for the first and second public IP addresses in outputs.tf

--- firewall/outputs.tf
+++ firewall/outputs.tf
@@ -7,6 +7,10 @@ output "firewall_resources_private_ip" {
   value = module.firewall.resource.virtual_hub[*].private_ip_address
 }
 
-output "firewall_resources_public_ip" {
-  value = module.firewall.resource.virtual_hub[*].public_ip_addresses
+output "firewall_resources_public_ip_one" {
+  value = module.firewall.resource.virtual_hub[*].public_ip_addresses[0]
+}
+
+output "firewall_resources_public_ip_two" {
+  value = module.firewall.resource.virtual_hub[*].public_ip_addresses[1]
 }
