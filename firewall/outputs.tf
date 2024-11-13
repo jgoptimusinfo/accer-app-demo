8de0b0c22a59f426e661eb865be98d0cd6d09b1c
jgoptimusinfo <javier.gaibisso@optimusinfo.com>
2024-11-13 15:56:44 -0300

    Remove commented-out output definitions in outputs.tf

--- firewall/outputs.tf
+++ firewall/outputs.tf
@@ -1,13 +1,4 @@
-/* 
+
 output "firewall_resources" {
   value = module.firewall.resource.virtual_hub[*]
 }
-
-output "firewall_private_ip" {
-  value = module.firewall.resource.virtual_hub[*].firewall_resources[0].private_ip_address
-}
-
-output "firewall_public_ip" {
-  value = module.firewall.resource.virtual_hub[0].firewall_resources[0].public_ip_addresses
-} 
-*/
