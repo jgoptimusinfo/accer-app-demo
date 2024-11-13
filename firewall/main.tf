2dc19e83759b8f6c5b0ca9ed3e50c580be9d4dbb
jgoptimusinfo <javier.gaibisso@optimusinfo.com>
2024-11-13 13:08:07 -0300

    Remove destination addresses from NAT rule configuration

--- firewall/main.tf
+++ firewall/main.tf
@@ -82,7 +82,7 @@ module "rule_collection_group" {
           translated_address    = "172.16.1.5"
           translated_port       = 443
           source_addresses      = ["*"]
-          destination_addresses = ["*"]
+          #destination_addresses = ["*"]
           destination_ports     = ["443"]
           protocols             = ["TCP"]
         }
