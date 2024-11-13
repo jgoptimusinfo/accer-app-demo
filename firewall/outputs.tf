9f97f991d8d5f619f67dea7edd3d1e0478aa9538
jgoptimusinfo <javier.gaibisso@optimusinfo.com>
2024-11-13 14:44:44 -0300

    Refactor firewall outputs to use updated resource references and remove commented code

--- firewall/outputs.tf
+++ firewall/outputs.tf
@@ -1,23 +1,11 @@
 output "firewall_resources" {
   value = module.firewall.resource.virtual_hub
 }
-/* 
-output "firewall_policy" {
-  value = module.firewall_policy.resource
-}
 
 output "firewall_private_ip" {
-  value = module.firewall.resource.virtual_hub.private_ip_address
+  value = module.firewall.resource.virtual_hub.firewall_resources[0].private_ip_address
 }
 
 output "firewall_public_ip" {
-  value = module.firewall.resource.virtual_hub.public_ip_addresses
+  value = module.firewall.resource.virtual_hub.firewall_resources[0].public_ip_addresses
 }
-
-output "firewall_public_ip_1" {
-  value = module.firewall.resource.virtual_hub.public_ip_addresses[0]
-}
-
-output "firewall_public_ip_2" {
-  value = module.firewall.resource.virtual_hub.public_ip_addresses[1]
-} */
