7b86714cdc0e03af56f7d7f98adf486aefa48fe4
jgoptimusinfo <javier.gaibisso@optimusinfo.com>
2024-11-13 14:48:20 -0300

    Update firewall output references to use correct module structure and comment out public IP output

--- firewall/outputs.tf
+++ firewall/outputs.tf
@@ -3,9 +3,9 @@ output "firewall_resources" {
 }
 
 output "firewall_private_ip" {
-  value = module.firewall.resource.virtual_hub.firewall_resources[0].private_ip_address
+  value = module.firewall.resource.virtual_hub[0].firewall_resources
 }
 
-output "firewall_public_ip" {
-  value = module.firewall.resource.virtual_hub.firewall_resources[0].public_ip_addresses
-}
+# output "firewall_public_ip" {
+#   value = module.firewall.resource.virtual_hub[0].firewall_resources[0].public_ip_addresses
+# }
