770f3196ae45add133b99f0205a6916301e9b17c
jgoptimusinfo <javier.gaibisso@optimusinfo.com>
2024-11-13 13:55:20 -0300

    Update destination address reference in DNAT rule configuration and add outputs for firewall IPs

--- firewall/outputs.tf
+++ firewall/outputs.tf
@@ -5,3 +5,19 @@ output "firewall_resources" {
 output "firewall_policy" {
   value = module.firewall_policy.resource
 }
+
+output "firewall_private_ip" {
+  value = module.firewall.resource.virtual_hub.private_ip_address
+}
+
+output "firewall_public_ip" {
+  value = module.firewall.resource.virtual_hub.public_ip_addresses
+}
+
+output "firewall_public_ip_1" {
+  value = module.firewall.resource.virtual_hub.public_ip_addresses[0]
+}
+
+output "firewall_public_ip_2" {
+  value = module.firewall.resource.virtual_hub.public_ip_addresses[1]
+}
